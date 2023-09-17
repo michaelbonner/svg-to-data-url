289b28e1130c3173ad82b35f745e62202afc0cdc
Michael Bonner <michaelwbonner@gmail.com>
2023-09-17 12:11:33 -0600

    real basic svg enoding implementation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="px-8 max-w-5xl mx-auto">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let svg = '';
+	let encodedSvg = '';
+	let svgBackground = '';
+
+	function handleInput(event: any) {
+		svg = event.target.value;
+		encodedSvg = encodeURIComponent(svg);
+		svgBackground = `url("data:image/svg+xml,${encodedSvg}")`;
+	}
+</script>
+
+<h1 class="text-3xl">SVG to data url</h1>
+
+<textarea class="w-full h-64 border" bind:value={svg} on:input={handleInput} />
+
+{#if svg}
+	<div class="bg-gray-50 p-4 inline-flex my-2">
+		<div class="svg-display" style="background-image: {svgBackground}" />
+	</div>
+
+	<div class="my-2">
+		<h2>Encoded SVG</h2>
+		<pre>{encodedSvg}</pre>
+	</div>
+
+	<div class="my-2">
+		<h2>SVG Background</h2>
+		<pre>{svgBackground}</pre>
+	</div>
+{/if}
+
+<style>
+	.svg-display {
+		width: 22px;
+		height: 22px;
+	}
+</style>
